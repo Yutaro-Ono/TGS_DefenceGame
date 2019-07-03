cbffef0d0932f5adf5d37c08c2296ae22046341c
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-07-03 16:21:53 +0900

    エネミーマネージャーを更新。ひとまずの目標は、一定秒数毎にエネミーをフィールドに沿うよう円状に出現させること

--- Source/EnemyManager.cpp
+++ Source/EnemyManager.cpp
@@ -3,3 +3,25 @@
 //                                              Last Update : 2019/07/03
 //-----------------------------------------------------------------------+
 #include "EnemyManager.h"
+
+const int EnemyManager::MAX_ENEMY_NUM = 5;           // 敵の一度に出現する最大数
+
+void EnemyManager::Initialize()
+{
+	// エネミーモデルの読み込み
+	m_enemySourceHandle = MV1LoadModel("Data/Model/Actor/Enemy/");
+
+
+}
+
+void EnemyManager::Delete()
+{
+}
+
+void EnemyManager::Update()
+{
+}
+
+void EnemyManager::Draw()
+{
+}
--- Source/EnemyManager.h
+++ Source/EnemyManager.h
@@ -4,10 +4,33 @@
 //                                  Last Update : 2019/07/03
 //-----------------------------------------------------------+
 #pragma once
+#include <iostream>
+#include <vector>
 #include "DxLib.h"
 #include "Enemy.h"
 
+
 class EnemyManager final
 {
+public:
+
+	void Initialize();                  // 各種初期化(モデルの読み込み、出現位置の設定)
+
+	void Delete();                      // 各種解放処理
+
+	void Update();                      // 更新処理
+
+	void Draw();                        // 描画処理
+
+
+private:
+
+	int m_enemySourceHandle;            // エネミーモデルのハンドル
+
+	std::vector<Actor*> enemy;
+
+
+	static const int MAX_ENEMY_NUM;     // 敵の一度に出現する最大数
+
 
 };
